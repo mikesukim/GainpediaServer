849595756ed2c76bce4052bdf5583c8d45b7cfce
Michael Kim <mikesungunkim@gmail.com>
2020-05-15 03:54:57 -0500

    detach schema

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
-const { ApolloServer, gql } = require('apollo-server');
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const { ApolloServer,gpl } = require('apollo-server');
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
- 
 const resolver = require('./resolver.js')
+const schema = require('./schema.js')
 const resolvers = resolver.resolver
-
+const typeDefs = schema.typeDefs
 
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers,
-  context: ({ request }) => {
-    console.log(request.headers)
-  }
  });
 exports.server = server
 
 // COMMENT IN DEPLOYMENT MODE
-// // The `listen` method launches a web server.
-// server.listen().then(({ url }) => {
-//   console.log(`🚀  Server ready at ${url}`);
-// });
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
